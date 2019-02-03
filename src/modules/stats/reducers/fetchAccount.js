9116dd6acd5700108127f6955271cc629f9d939a
Jason <11766330+jreala@users.noreply.github.com>
2019-02-02 22:49:44 -0800

    Restructure reducers to return early on invalid data

--- src/modules/stats/reducers/fetchAccount.js
+++ src/modules/stats/reducers/fetchAccount.js
@@ -4,14 +4,17 @@ import HttpClient from '../../../lib/httpClient';
 
 const fetchAccount = (state, action) => {
     const data = HttpClient.validateData(action.data);
+
+    if (!data.Valid) {
+        return state;
+    }
+
     const newState = assign({}, state);
 
-    if (data.Valid) {
-        map(data.Response, (value) => {
-            newState.accounts[value.membershipId] = value;
-            newState.memberIdByName[encodeURIComponent(value.displayName).toLowerCase()] = value.membershipId;
-        });
-    }
+    map(data.Response, (value) => {
+        newState.accounts[value.membershipId] = value;
+        newState.memberIdByName[encodeURIComponent(value.displayName).toLowerCase()] = value.membershipId;
+    });
 
     return {
         ...newState
--- src/modules/stats/reducers/fetchProfile.js
+++ src/modules/stats/reducers/fetchProfile.js
@@ -4,14 +4,17 @@ import HttpClient from '../../../lib/httpClient';
 
 const fetchProfile = (state, action) => {
     const data = HttpClient.validateData(action.data);
+
+    if (!data.Valid) {
+        return state;
+    }
+
     const newState = assign({}, state);
 
-    if (data.Valid) {
-        newState.profiles[data.Response.profile.data.userInfo.membershipId] = data.Response.profile.data;
-        map(data.Response.profile.data.characterIds, (characterId) => {
-            newState.characters[characterId] = null;
-        });
-    }
+    newState.profiles[data.Response.profile.data.userInfo.membershipId] = data.Response.profile.data;
+    map(data.Response.profile.data.characterIds, (characterId) => {
+        newState.characters[characterId] = null;
+    });
 
     return {
         ...newState
