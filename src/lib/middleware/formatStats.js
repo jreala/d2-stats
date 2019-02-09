ab79b89e3cef5cc6b7ddacf18f06ad9b886ebebf
Jason <11766330+jreala@users.noreply.github.com>
2019-02-08 22:01:51 -0800

    Fill out middleware for stats and clean up reducer

--- src/lib/middleware/formatStats.js
+++ src/lib/middleware/formatStats.js
@@ -1,6 +1,16 @@
 import assign from 'lodash/assign';
+import map from 'lodash/map';
+import reduce from 'lodash/reduce';
+import StatId from '../statId';
 import { FORMAT_TYPE_STATS } from '../constants';
 import HttpClient from '../httpClient';
+
+const extract = collection => reduce(collection, (result, value, key) => {
+    if (StatId.includes(key)) {
+        result[key] = value;
+    }
+    return result;
+}, {});
 
 // eslint-disable-next-line no-unused-vars
 const formatCharacterStats = store => next => (action) => {
@@ -9,15 +19,29 @@ const formatCharacterStats = store => next => (action) => {
         return next(action);
     }
 
-    const data = HttpClient.validateData(action.data);
+    const parsed = HttpClient.validateData(action.data);
 
-    if (!data.Valid) {
+    if (!parsed.Valid) {
         return next(action);
     }
 
-    const mydata = assign({}, action, data);
+    const { data, ...rest } = action;
+    const statsByCharacter = {};
+    const overallStats = {
+        pvp: extract(parsed.Response.mergedAllCharacters.results.allPvP.allTime),
+        pve: extract(parsed.Response.mergedAllCharacters.results.allPvE.allTime),
+        merged: extract(parsed.Response.mergedAllCharacters.merged.allTime)
+    };
 
-    return next(mydata);
+    map(parsed.Response.characters, (character) => {
+        const pvp = extract(character.results.allPvP.allTime);
+        const pve = extract(character.results.allPvE.allTime);
+        const merged = extract(character.merged.allTime);
+
+        statsByCharacter[character.characterId] = { pvp, pve, merged };
+    });
+
+    return next(assign({}, rest, { statsByCharacter }, { overallStats }));
 };
 
 export default formatCharacterStats;
--- src/modules/stats/reducers/fetchStats.js
+++ src/modules/stats/reducers/fetchStats.js
@@ -1,32 +1,9 @@
 import assign from 'lodash/assign';
-import HttpClient from '../../../lib/httpClient';
 
 const fetchStats = (state, action) => {
-    const data = HttpClient.validateData(action.data);
-
-    console.log(data);
-
-    if (!data.Valid) {
-        return state;
-    }
-
     const newState = assign({}, state);
-    const mergedAllCharacters = data.Response.mergedAllCharacters;
-    const [character1, character2, character3] = data.Response.characters;
-
-    newState.accountStats[action.membershipId] = mergedAllCharacters;
-
-    if (character1) {
-        newState.characterStats[character1.characterId] = character1;
-    }
-
-    if (character2) {
-        newState.characterStats[character2.characterId] = character2;
-    }
-
-    if (character3) {
-        newState.characterStats[character3.characterId] = character3;
-    }
+    newState.accountStats[action.membershipId] = action.overallStats;
+    newState.characterStats = assign(newState.characterStats, action.statsByCharacter);
 
     return {
         ...newState
